71182f9522f4e4bfe0cd6d5396accd6855d2a57a
Numair Khan <nummuzummu@gmail.com>
2026-09-02 10:58:16 +0530

    priority questions

---
+++ bankQueue.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class bankQueue {
+    public static void main(String[] args) {
+
+        Queue<Integer> customers = new LinkedList<>();
+
+        customers.add(101);
+        customers.add(102);
+        customers.add(103);
+        customers.add(104);
+        customers.add(105);
+
+        while (!customers.isEmpty()) {
+            int customer = customers.poll();//delete first element from queue
+            System.out.println("Customer " + customer + " is being served.");
+        }
+        
+        if (customers.isEmpty()) {
+            System.out.println("No customer is waiting");
+        }
+    }
+}
